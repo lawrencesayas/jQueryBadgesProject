c61601d7706633cb3075c6ec97f315530f43e413
lawrencesayas <lawrencesayas@gmail.com>
2017-02-09 21:24:53 +1300

    Solved Prjoect
    Solution to project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,39 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/lawrencesayas.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      showCourses(response.courses.completed);
+    }
+  });
+
+  function showCourses(courses){
+    console.log(courses);
+
+    courses.forEach(function(course){
+
+      var $badges = '#badges'
+
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        href: course.url,
+        target: '_blank',
+        'class': 'btn btn-primary',
+        text: 'See Course'
+      }).appendTo($course);
+    })
+  }
 
 });
